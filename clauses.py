c1796756e9552ef09f557a734abe52f2ee3d9c0f
namlose <ahbehnomics@gmail.com>
2020-05-16 20:00:40 +0100

    .
    .

---
+++ clauses.py
@@ -0,0 +1,26 @@
+
+print("Insert a number: \n")
+value = input()
+
+if value == "7":
+    print("Value is 7")
+elif value == "8":
+    print("Value is 8")
+elif value > "10":
+    print("Value is bigger or equal to 10")
+elif value < "10":
+    print("Value is smaller than 10")
+elif value != "7":
+    print("Value is not 7")
+
+
+
+first_value = True
+second_value = "6"
+if first_value:
+    if second_value == "6":
+        print("Ok!")
+
+    
+    
+print ("Finished!")
