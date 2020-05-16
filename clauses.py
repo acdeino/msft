7fc0fb5d99e05adf33d504c303b352378a71c6b2
namlose <ahbehnomics@gmail.com>
2020-05-16 20:12:17 +0100

    ..
    .

--- clauses.py
+++ clauses.py
@@ -12,15 +12,25 @@ elif value < "10":
     print("Value is smaller than 10")
 elif value != "7":
     print("Value is not 7")
-
+else:
+     print ("Hello")
 
 
 first_value = True
-second_value = "6"
+second_value6 = "6"
 if first_value:
     if second_value == "6":
         print("Ok!")
+        
 
-    
-    
+print(type("Hello World!"))
+print(type("7"))
+print(type("18.456"))
+
+print(type(True))    
+print(type(False))
+
+print(type('True'))
+print(type('Falso'))
+
 print ("Finished!")
