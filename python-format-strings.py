e6ba3d53c43215ecb6eba1ce1410b6eedea4c5b7
namlose <ahbehnomics@gmail.com>
2020-05-18 19:32:07 +0100

    modified:   python-format-strings.py 	helperfunctions.py

--- python-format-strings.py
+++ python-format-strings.py
@@ -3,6 +3,7 @@ second_string = "Hello"
 
 print(first_string == second_string)
 
+print("\n")
 
 third_string = 'A single quoted literal string with a " (double) quote'
 fourth_string = "A double quoted literal string with a '(single) quote"
@@ -10,6 +11,8 @@ fourth_string = "A double quoted literal string with a '(single) quote"
 print(third_string)
 print(fourth_string)
 print(third_string == fourth_string)
+
+print("\n")
 
 fifth_string = 'A single quoted literal string with an \' escaped single quote'
 sixth_string = "A double quoted literal string with an \' escaped double quote"
@@ -21,10 +24,13 @@ print(sixth_string)
 print(seventh_string)
 print(eighth_string)
 
+print("\n")
+
 nineth_string = r"A literal string with a \n line character print raw"
 
 print(nineth_string)
 
+print("\n")
 
 tenth_string = ''' A literal string
 or more than one line,
@@ -33,5 +39,19 @@ eleventh_string = """Another literal fifth_string
     on more than one line,
 using double quotes"""
 
-print(tenth_string)
+print(tenth_string, "\n")
 print(eleventh_string)
+
+print("\n")
+
+first_name = 'Conrad'
+second_name = 'Grant'
+third_name = 'Bob'
+
+print(first_name, second_name)
+print(first_name + second_name, third_name)
+print(first_name, second_name, third_name)
+print(first_name, second_name, third_name, sep=" - ")
+print(first_name, second_name, third_name, sep=" - ", end=".")
+
+print("\n")
