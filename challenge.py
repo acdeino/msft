5ed9576368b24e79de1b80f1ac36f4296ca091dc
namlose <ahbehnomics@gmail.com>
2020-05-18 21:39:43 +0100

    .
    .

---
+++ challenge.py
@@ -0,0 +1,10 @@
+
+value = input ("Would you like to continue? ")
+
+if value == "y" or value == "yes":
+    print("Processing...\n")
+    print("Completed!")
+elif value == "n" or value == "no":
+    print("Goodbye!")
+else:
+    print("Try Again!")
