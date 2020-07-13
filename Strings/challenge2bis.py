a931f7324867299ddf66ccf78ca737f22dd3d6ae
Namlose <ahbehnomics@gmail.com>
2020-07-13 14:13:40 +0100

    .

--- Strings/challenge2bis.py
+++ Strings/challenge2bis.py
@@ -19,16 +19,17 @@ second_value = second_value.capitalize()
 
 # Third challenge
 third_value = third_value.replace(" ", "")
-third_value = third_value.replace('-', ' ')
+third_value = third_value.replace("-", " ")
 third_value = third_value.swapcase()
 third_value = f'{third_value:>30}'
+
 
 print(first_value)
 print(second_value)
 print(third_value)
 
 # Fourth challenge - use only the print() function (no f-strings)
-print(fourth_value, fifth_value, sixth_value, sep='#', end="!")
+print(fourth_value, fifth_value, sixth_value, sep="#", end="!")
 
 # Fifth challenge - use only a single print() function.  Create tabs and new lines using f-strings.
 print(f'\n\t{fourth_value}\n\t{fifth_value}\n\t{sixth_value}')
