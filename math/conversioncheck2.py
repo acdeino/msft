3af56522adb7be6adb20cfab37e5600ff863b480
Namlose <ahbehnomics@gmail.com>
2020-07-14 04:02:13 +0100

    .

--- math/conversioncheck2.py
+++ math/conversioncheck2.py
@@ -1,7 +1,7 @@
 first_value = input("First Number is: ")
 second_value = input("Second number is: ")
 
-if first_value.isnumeric() == False or second_value.isnumeric() == False
+if first_value.isnumeric() == False or second_value.isnumeric() == False:
     print("Please Enter numbers only.")
     exit()
 
@@ -9,4 +9,4 @@ first_value = int(first_value)
 second_value = int(second_value)
 
 sum = first_value + second_value
-print("The sum is: " + sum)
+print("The sum is: " + str(sum))
